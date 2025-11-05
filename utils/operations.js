7a23da4530a97ba887b2ce159d7e7545fe4ff841
Vuir <Taevio@hotmail.com>
2025-11-05 11:31:38 +0300

    Todo 4

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,20 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((total, num) => total - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((total, num) => total * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  return 
+  if (numbers.slice(1).some((num) => num === 0)) {
+    return NaN;
+  }
+  return numbers.slice(1).reduce((total, num) => total / num, numbers[0]);
 }
 
