94ea714a2e4e1c8fa53d16a8beb14be71a024686
Vuir <Taevio@hotmail.com>
2025-11-05 11:33:42 +0300

    Todo 5

--- utils/parser.js
+++ utils/parser.js
@@ -1,10 +1,14 @@
 import _ from "lodash";
 
 export function parseNumbers(input) {
-  return {/*write code here*/}
+  return 
+  const numbers = _.map(input, (str) => Number(str));
+    return _.compact(numbers);
 }
 
 export function isValidOperation(operation) {
-  return {/*write code here*/}
+  return 
+  const validOps = ["add", "subtract", "multiply", "divide"];
+    return _.includes(validOps, operation);
 }
 
